a683f6255d6491405cd90404648095a88f49dfa8
B126 <C0B2212694@edu.teu.jp>
2023-06-20 17:08:16 +0900

    kadai

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,14 +2,41 @@ import random
 import sys
 import pygame as pg
 
-
+#kadai何の成果もなし
 WIDTH, HEIGHT = 1600, 900
-delta={
-    pg.K_UP:(0,-5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5,0),
-    pg.K_RIGHT:(+5,0),
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
 }
+
+"""
+kaiten_kk_img={
+    pg.transform.flip(kk_img,-45),
+    pg.transform.flip(kk_img,0),
+    pg.transform.flip(kk_img,45),
+    pg.transform.flip(kk_img,90),
+    pg.transform.flip(kk_img,-45),
+    pg.transform.flip(kk_img,0),
+    pg.transform.flip(kk_img,45),
+    pg.transform.flip(kk_img,90),  
+}
+"""
+
+def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect，爆弾Rectが画面外 or 画面内かを判定する関数
+    引数：こうかとんRect or 爆弾Rect
+    戻り値：横方向，縦方向の判定結果タプル（True：画面内／False：画面外）
+    """
+    yoko, tate = True, True
+    if rect.left< 0 or WIDTH < rect.right:  # 横方向判定
+        yoko = False
+    if rect.top-40 < 0 or HEIGHT -40 < rect.bottom:  # 縦方向判定
+        tate = False
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -17,41 +44,64 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    kk_rct=kk_img.get_rect()
-    kk_rct.center=900,400
-
-    bd=pg.Surface((20,20))
-    bd.set_colorkey((0,0,0))
-    pg.draw.circle(bd,(255,0,0),(10,10),10)
+    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+    bd_img = pg.Surface((20,20))  # 練習１
+    bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0,0), (10, 10), 10)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
+    bd_rct = bd_img.get_rect()
+    # 爆弾Rectの中心座標を乱数で指定する
+    bd_rct.center = x, y 
+    vx, vy = +5,+5  # 練習２
+    wx,wy=0.2,0.2 #kadai2.大きくなる
     
-    x=random.randint(0,WIDTH)
-    y=random.randint(0,HEIGHT)
-
-    bd_rect=bd.get_rect()
-    bd_rect.center=x,y
-    vx,vy=+5,+5
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
-                return 
-        key_lst=pg.key.get_pressed()
-        sum_mv=[0,0]
+                return
+    #colorかえたい
+        bd_img = pg.Surface((20,20))  # 練習１
+        bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
+        #課題5：爆弾の色を変えた
+        #pg.draw.circle(bd_img, (25*tmr%255, (255-tmr)%255,(30*tmr-tmr*2)%255), (10, 10), 10)
+        if kk_rct.colliderect(bd_rct): # 練習５
+            pg.draw.circle(kk_img,(0,0,0),)
+            print("ゲームオーバー")
+            return   #ゲームオーバー 
+        
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
         for k, mv in delta.items():
-            if key_lst[k]:
-                sum_mv[0]+=mv[0]
-                sum_mv[1]+=mv[1]
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+                
         kk_rct.move_ip(sum_mv)
-
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+ 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
-        bd_rect.move_ip(vx,vy)
-        screen.blit(bd,bd_rect)
+        bd_rct.move_ip(vx, vy)  # 練習２
+        #bd_rct.size(20+tmr,20+tmr)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 横方向に画面外だったら
+            vx *= -1
+        if not tate:  # 縦方向に範囲外だったら
+            vy *= -1
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+      
+        clock.tick(50)
 
 
 if __name__ == "__main__":
